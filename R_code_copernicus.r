0da3668cb01e6facc85c7df9124b7ae46c399a70
FedericoTossani <84522543+FedericoTossani@users.noreply.github.com>
2021-06-02 17:01:58 +0200

    Update R_code_copernicus.r

--- R_code_copernicus.r
+++ R_code_copernicus.r
@@ -6,3 +6,21 @@ library(raster)
 library(ncdf4)
 
 setwd("/Users/federicotossani/lab/")
+
+#prima di importare l'immagine l'ho rinominata.
+NDVI <- raster("NDVI_202006010.nc")
+NDVI
+
+#la risoluzione è in gradi, non in metri, sono quindi coordinate geografiche.
+#il sistema di riferimento è il WGS84
+
+cl<-colorRampPalette(c("brown", "yellow", "green", "darkgreen")) (200)
+plot(NDVI, col=cl)
+
+#questo dato si può ricampionare per ottenerne uno con pixel più grandi e alleggerirlo.
+#per ricampionare l'immagine uso la funzione aggregate
+
+NDVIres<-aggregate(NDVI, fact=100)
+NDVIres
+
+#questo tipo di ricampionamento di chiama bilineare, prende 2 line di pixel e fa la media dei pixel all'interno.
