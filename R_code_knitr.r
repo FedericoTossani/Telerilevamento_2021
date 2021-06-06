271ffd4e994927364f177ddc39670840b36c1274
FedericoTossani <84522543+FedericoTossani@users.noreply.github.com>
2021-06-06 17:49:01 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -7,4 +7,8 @@ setwd("/Users/federicotossani/lab/")
 
 stitch("R_code_greenland.txt", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
 
-#riprendi dal 1:08:00 del 16/04
+#prima di tutto apro il codice di cui mi interessa fare il report e lo salvo nella cartella (lab ne nostro caso).
+#il primo argomento di stich è il nome del file contenente il codice e la sua estensione.
+#in questo modo leggerà automaticamente tutto il codice e creerà il report.
+#il template è l'altro argomento che ci ha fornito riettamente il prof
+#l'ultimo è il tipo di pacchetto che andiamo ad utilizzare
