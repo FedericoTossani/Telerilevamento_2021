1776a359e89d9a53ca9afecd3d4a3cdaad38c64b
FedericoTossani <84522543+FedericoTossani@users.noreply.github.com>
2021-06-12 12:12:51 +0200

    Update R_code_variability.r

--- R_code_variability.r
+++ R_code_variability.r
@@ -28,24 +28,29 @@ red<-sentinel$sentinel.2
 ndvi<-(nir-red)/(nir+red)
 plot(ndvi)
 #dove vediamo il bianco non c'è vegetazione, nel marroncino c'è la roccia, nel giallino/verdino sono le parti di bosco e il verde scuro sono le praterie
-cl <- colorRampPalette(c("green","yellow","orange","red","white", "black"))(100)
+cl <- colorRampPalette(c("dark green","yellow","orange","red","white", "black"))(100)
 plot(ndvi, col=cl)
 
 #questo plot ci permette di apprezzare i diversi livelli di ndvi.
 
-ndvi_sd3<-focal(ndvi,w=matrix(1/9, nrow=3, ncol=3), fun=sd)
+ndvi_sd3 <- focal(ndvi,w=matrix(1/9, nrow=3, ncol=3), fun=sd)
 #la funzione focal mi permette di fare diversi calcoli statistici per mezzo della moving window
 #gli argomenti nella funzione sono: il nome dell'immagine, la matrice (è la moving window) questa più è grande più pixel prende in considerazione e più lungo sarà il calcolo.
 #1/9 significa che prendi in considerazione ogni pixel sui 9 della matrice.
 
 plot(ndvi_sd3,col=cl)
 
-#####################
-# lez 19/05 min 1:00:00
+#la colorazione ci da un'idea della distribuzione della deviazione standard nella nostra immagine.
+#la sd è più bassa nella parti più omogenee, mentra aumenta nelle zone dove si passa da roccia nuda a parte vegetata. Torna a diminuire nelle zone di prateria, in quanto più omogenee.
+#ci sono piccole zone in aumento che corrispondono ai picchi più alti e crepacci.
 
+ndvi_m3 <- focal(ndvi,w=matrix(1/9, nrow=3, ncol=3), fun=mean)
+plot(ndvi_m3,col=cl)
 
+#in questo caso è lo stesso processo descritto precendentemente solo che la moving window calcola la media e non la deviazione standard.
 
-
+ndvi_sd9 <- focal(ndvi,w=matrix(1/81, nrow=9, ncol=9), fun=sd)
+plot(ndvi_sd9,col=cl)
 
 
 
